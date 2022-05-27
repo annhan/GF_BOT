32dd96f006ec25e813a9a8011675b38d398a411d
NhanPham <annhandt09@gmail.com>
2022-05-27 22:01:40 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,30 @@
+timebegin = 1653483600
+contract_address = "0xe952c73166623fec0c871abd4ffb6e215adb1f67" #be sure to use a BSC Address in uppercase format like this 0x9F0818B... 
+pool_id = 154
+
+gas = 200000
+gwei = 10
+
+CAPMONTER_API = "" # API of CAPMONTER
+max_buy = 70*1000000000000000000
+
+list_account = [
+                    {
+                        "stt": "1",
+
+                        "account_params": {
+                            "myaddress": "0x111111111",
+                            "private_key": "",
+                        }
+                    },
+
+                    {
+
+                        "stt": "2",
+
+                        "account_params": {
+                            "myaddress": "0x2222222",
+                            "private_key": "",
+                        }
+                    }   
+                ]
